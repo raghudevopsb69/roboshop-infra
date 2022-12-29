126e995641df706443e40ff9b4852cedd6f480f2
Raghu K <raghuk.vit@gmail.com>
2022-12-29 08:50:17 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -40,13 +40,13 @@ resource "aws_security_group" "allow_tls" {
 }
 
 data "aws_route53_zone" "domain" {
-  name         = var.DOMAIN_NAME
+  name = var.DOMAIN_NAME
 }
 
 resource "aws_route53_record" "dns-record" {
-  count = length(var.instances)
+  count   = length(var.instances)
   zone_id = data.aws_route53_zone.domain.zone_id
-  name    = "${var.ENV}-${element(var.instances, count.index).${var.DOMAIN_NAME}}"
+  name    = "${var.ENV}-${element(var.instances, count.index)}.${var.DOMAIN_NAME}"
   type    = "A"
   ttl     = 30
   records = [element(aws_instance.ec2.*.private_ip, count.index)]
