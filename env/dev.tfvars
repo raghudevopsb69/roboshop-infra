8c5e27915561fbb4221d7db6a79ff2140a465b48
Raghu K <raghuk.vit@gmail.com>
2023-01-03 08:15:58 +0530

    Update roboshop-infra/

--- env/dev.tfvars
+++ env/dev.tfvars
@@ -9,9 +9,11 @@ default_vpc_route_table = "rtb-06dc24aa66c02de3b"
 
 vpc = {
   dev = {
-    vpc_cidr_block            = "10.0.0.0/16"
-    public_subnet_cidr_block  = ["10.0.0.0/24", "10.0.1.0/24"]
-    private_subnet_cidr_block = ["10.0.2.0/24", "10.0.3.0/24"]
-    subnet_azs                = ["us-east-1a", "us-east-1b"]
+    vpc_cidr_block           = "10.0.0.0/16"
+    public_subnet_cidr_block = ["10.0.0.0/24", "10.0.1.0/24"]
+    app_subnet_cidr_block    = ["10.0.2.0/24", "10.0.3.0/24"]
+    db_subnet_cidr_block     = ["10.0.4.0/24", "10.0.5.0/24"]
+    subnet_azs               = ["us-east-1a", "us-east-1b"]
   }
 }
+
--- main.tf
+++ main.tf
@@ -5,10 +5,11 @@ module "vpc" {
   default_vpc_cidr_block  = var.default_vpc_cidr_block
   default_vpc_route_table = var.default_vpc_route_table
 
-  for_each                  = var.vpc
-  vpc_cidr_block            = each.value.vpc_cidr_block
-  public_subnet_cidr_block  = each.value.public_subnet_cidr_block
-  private_subnet_cidr_block = each.value.private_subnet_cidr_block
-  subnet_azs                = each.value.subnet_azs
+  for_each                 = var.vpc
+  vpc_cidr_block           = each.value.vpc_cidr_block
+  public_subnet_cidr_block = each.value.public_subnet_cidr_block
+  app_subnet_cidr_block    = each.value.app_subnet_cidr_block
+  db_subnet_cidr_block     = each.value.db_subnet_cidr_block
+  subnet_azs               = each.value.subnet_azs
 }
 
